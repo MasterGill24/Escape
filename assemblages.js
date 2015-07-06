3b2f81bceb93b1da80b5e7ff221b1083ac04fd9e
Asma <asmavajih@gmail.com>
2015-07-06 14:03:42 -0700

    Created assemblages

---
+++ assemblages.js
@@ -0,0 +1,50 @@
+ECS.Assemblages = {
+	Player: function(){
+		var entity = new ECS.Entity();
+		entity.addComponent(new ECS.Components.Position());
+		entity.addComponent(new ECS.Components.Velocity());
+		entity.addComponent(new ECS.Components.Health());
+		entity.addComponent(new ECS.Components.Collision());
+		entity.addComponent(new ECS.Components.Spritesheet());
+		entity.addComponent(new ECS.Components.PlayerControlled());
+		return entity;
+	}
+	Gun: function(){
+		var entity = new ECS.Entity();
+		entity.addComponent(new ECS.Components.Position());
+		entity.addComponent(new ECS.Components.Spritesheet());
+		return entity;
+	}
+	Bullet: function(){
+		var entity = new ECS.Entity();
+		entity.addComponent(new ECS.Components.Position());
+		entity.addComponent(new ECS.Components.Velocity());
+		entity.addComponent(new ECS.Components.Damage());
+		entity.addComponent(new ECS.Components.Collision());
+		entity.addComponent(new ECS.Components.Spritesheet());
+		return entity;
+	}
+	Enemy: function(){
+		var entity = new ECS.Entity();
+		entity.addComponent(new ECS.Components.Position());
+		entity.addComponent(new ECS.Components.Velocity());
+		entity.addComponent(new ECS.Components.Health());
+		entity.addComponent(new ECS.Components.Damage());
+		entity.addComponent(new ECS.Components.Collision());
+		entity.addComponent(new ECS.Components.Spritesheet());
+		return entity;
+	}
+	BackgroundTile: function(){
+		var entity = new ECS.Entity();
+		entity.addComponent(new ECS.Components.Position());
+		entity.addComponent(new ECS.Components.Spritesheet());
+		return entity;
+	}
+	Tile: function(){
+		var entity = new ECS.Entity();
+		entity.addComponent(new ECS.Components.Position());
+		entity.addComponent(new ECS.Components.Collision());
+		entity.addComponent(new ECS.Components.Spritesheet());
+		return entity;
+	}
+}
