1b2530b40d5bab8845bbe138b25ac8e86d986c7e
Tyler Packard <tyler.packard86@gmail.com>
2015-07-06 13:38:39 -0700

    Improved vectors

--- vector.js
+++ vector.js
@@ -1,31 +1,38 @@
 var Vector = function() {
 	function Vector(x, y){
 		this.x = x;
-		this.y = y;	
-	} 
+		this.y = y;
+
+		Object.defineProperty(this, "magnitude", {
+			get: function() {
+				return Math.sqrt(this.x * this.x + this.y * this.y);
+			}
+		});
+	}
+
 	Vector.prototype.add = function(v){
 		this.x += v.x;
 		this.y += v.y;
 		return this;
 	}
-	
+
 	Vector.prototype.substract = function(v){
 		this.x -= v.x;
 		this.y -= v.y;
 		return this;
 	}
-	
+
 	Vector.prototype.scale = function(scalar){
 		this.x *= scalar;
 		this.y *= scalar;
 		return this;
 	}
-	
+
 	Vector.prototype.normalize = function(){
-		var magnitude = Math.sqrt(this.x * this.x + this.y * this.y);
+		var magnitude = this.magnitude;
 		this.scale(1/magnitude);
 		return this;
 	}
-	
+
 	return Vector;
 }();
