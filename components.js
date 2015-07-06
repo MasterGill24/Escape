f0bb227decace9c8e600a8c0e8875f2f4ea95850
Asma <asmavajih@gmail.com>
2015-07-06 10:30:56 -0700

    Added health and damage components

--- components.js
+++ components.js
@@ -2,7 +2,22 @@
 ECS.Components.Position = function(x, y) {
   this.x = x || 0;
   this.y = y || 0;
-
+	
   return this;
 }
 ECS.Components.Position.prototype.name = "Position";
+
+ECS.Components.Health = function (initialHealth, maxHealth){
+	this.initialHealth = initialHealth;
+	this.maxHealth = maxHealth;
+	
+	return this;
+}
+ECS.Components.Health.prototype.name = "Health";
+
+ECS.Components.Damage = function (damage){
+	this.damage = damage;
+	
+	return this:
+}
+ECS.Components.Damage.prototype.name = "Damage";
