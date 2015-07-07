e37d72483d5036d36d086850cee2e688fc483632
Tyler Packard <tyler.packard86@gmail.com>
2015-07-07 09:45:29 -0700

    Added comments to the movement system

--- systems/movement.js
+++ systems/movement.js
@@ -1,10 +1,14 @@
+/**
+ * The movement system moves the entity based off of its current velocity.
+ */
 ECS.Systems.Movement = function() {
   function Movement() {
     for (var entityId in ECS.Entities) {
       var entity = ECS.Entities[entityId];
       if (entity.components.Position && entity.components.Velocity) {
-		entity.components.Position.x += (entity.components.Velocity.x / 60.0);
-		entity.components.Position.y += (entity.components.Velocity.y / 60.0);		
+        // Divide the velocity by 60 since the game runs at 60 fps
+        entity.components.Position.x += (entity.components.Velocity.x / 60.0);
+        entity.components.Position.y += (entity.components.Velocity.y / 60.0);
       }
     }
   }
