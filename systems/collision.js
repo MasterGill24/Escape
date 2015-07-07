282689a1294b160a1ff263fd8b17364b0c56c663
Tyler Packard <tyler.packard86@gmail.com>
2015-07-07 09:38:43 -0700

    Removed callbacks from collisions

--- systems/collision.js
+++ systems/collision.js
@@ -9,7 +9,23 @@ ECS.Systems.Collision = function() {
            entity !== other &&
            (Math.abs(entity.components.Position.x - other.components.Position.x) * 2 < (entity.components.Collision.width + other.components.Collision.width)) &&
            (Math.abs(entity.components.Position.y - other.components.Position.y) * 2 < (entity.components.Collision.height + other.components.Collision.height))) {
-          entity.components.Collision.callback();
+          // Every entity now has a type stored in entity.components.Type.type
+          // Use it in to determine what to do in case of a collision
+
+          // If one is a player and one is an enemy
+          // Decrement the player's health by the enemy's damage
+
+          // If one is the player and one is a tile
+          // Stop the player
+
+          // If one is an enemy and one is a tile
+          // Turn the enemy around
+
+          // If one is a bullet and one is an enemy
+          // Remove the bullet and decrement the enemy's health by the bullet's damage
+
+          // If one is a bullet and one is a tile
+          // Remove the bullet
         }
       }
     }
