ca52a81097554b89dda9c952ebb5414e57be9d68
MasterGill24 <theshergill@hotmail.com>
2015-07-07 10:56:52 -0700

    Created Collision logic

--- systems/collision.js
+++ systems/collision.js
@@ -11,21 +11,35 @@ ECS.Systems.Collision = function() {
            (Math.abs(entity.components.Position.y - other.components.Position.y) * 2 < (entity.components.Collision.height + other.components.Collision.height))) {
           // Every entity now has a type stored in entity.components.Type.type
           // Use it in to determine what to do in case of a collision
-
+			
           // If one is a player and one is an enemy
           // Decrement the player's health by the enemy's damage
-
+			if (entity.components.Type.type === "player" && other.components.Type.type === "enemy") {
+				entity.components.Health.health -= entity.components.Damage.damage;
+			}
           // If one is the player and one is a tile
           // Stop the player
-
+			if (entity.components.Type.type === "player" && other.components.Type.type === "tile") {
+				//set velocity to 0
+				entity.components.Velocity.velocity = new Vector(0, 0);
+			}
           // If one is an enemy and one is a tile
           // Turn the enemy around
-
+			if (entity.components.Type.type === "enemy" && other.components.Type.type === "tile") {
+				//make velocity negative (x --> -x)
+				entity.components.Velocity.velocity.x = -entity.components.Velocity.velocity.x; 
+			}
           // If one is a bullet and one is an enemy
           // Remove the bullet and decrement the enemy's health by the bullet's damage
-
+			if (entity.components.Type.type === "bullet" && other.components.Type.type === "enemy") {
+				other.components.Health.health -= entity.components.Damage.damage;
+				delete entity;
+			}
           // If one is a bullet and one is a tile
           // Remove the bullet
+			if (entity.components.Type.type === "bullet" && other.components.Type.type === "tile") {
+				delete entity;
+			}
         }
       }
     }
