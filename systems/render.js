b49657e496fce6b53d90d9db87a365f4f7c22735
Tyler Packard <tyler.packard86@gmail.com>
2015-07-07 09:57:07 -0700

    Created render system

---
+++ systems/render.js
@@ -0,0 +1,37 @@
+ECS.Systems.Render = function() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  function Render() {
+    for (var entityId in ECS.Entities) {
+      var entity = ECS.Entities[entityId];
+
+      if (entity.components.Position && entity.components.Spritesheet) {
+        // Temporarily only drawing colored rectangles
+        switch (entity.components.Type.type) {
+          case "player":
+            ctx.fillStyle = "#00FF99";
+            break;
+          case "enemy":
+            ctx.fillStyle = "#FF2233";
+            break;
+          case "gun":
+            ctx.fillStyle = "#FFBB00";
+            break;
+          case "bullet":
+            ctx.fillStyle = "#EEEEEE";
+            break;
+          case "bg tile":
+            ctx.fillStyle = "#223344";
+            break;
+          case "tile":
+            ctx.fillStyle = "#0099FF";
+            break;
+        }
+
+        ctx.fillRect(entity.components.Position.x, entity.components.Position.y, entity.components.Spritesheet.width, entity.components.Spritesheet.height)
+      }
+    }
+  }
+
+  return Render;
+}();
