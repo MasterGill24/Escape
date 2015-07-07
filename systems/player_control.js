86ddaf0e8c8495ce87cfb31663d48236239a6e27
Tyler Packard <tyler.packard86@gmail.com>
2015-07-06 20:42:46 -0700

    Create player control system

---
+++ systems/player_control.js
@@ -0,0 +1,44 @@
+ECS.Systems.PlayerControl = function() {
+  var playerSpeed = 1;
+  var jumpPos = 0;
+  var jumping = false;
+
+  var zeroVector = new Vector(0, 0);
+  var leftVector = new Vector(-playerSpeed, 0);
+  var rightVector = new Vector(playerSpeed, 0);
+
+  function PlayerControl() {
+    for (var entityId in ECS.Entities) {
+      var entity = ECS.Entities[entityId];
+
+      if (entity.components.Velocity && entity.components.PlayerControl) {
+        entity.components.Velocity.velocity = zeroVector.clone();
+        if (Input.isKeyDown(document.body, "left")) {
+          entity.components.Velocity.velocity.add(leftVector);
+        }
+        if (Input.isKeyDown(document.body, "right")) {
+          entity.components.Velocity.velocity.add(rightVector);
+        }
+        if (Input.isKeyDown(document.body, "z")) {
+          jumping = true;
+        }
+        if (Input.isKeyDown(document.body, "x")) {
+          // Shoot
+        }
+
+        if (jumping) {
+          // Calculate jump vector
+          var jumpY = -(jumpPos*jumpPos) + 4 * jumpPos;
+          if (jumpPos < 4) {
+            entity.components.Velocity.velocity.add(newVector(0, jumpY));
+          } else {
+            jumping = false;
+            jumpPos = 0;
+          }
+        }
+      }
+    }
+  }
+
+  return PlayerControl;
+}();
